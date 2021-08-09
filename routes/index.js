cbc73a17b332e6c374d6f1f979c5277a4d18d6e1
Thomas Li <thomasligreat11@gmail.com>
2021-08-08 18:02:15 -0700

    changed logs

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,8 @@ router.head('/card', function(req, res, next){
 });
 
 router.post('/card', async function(req, res, next){
-  console.log(req.body.action.data.card);
+  console.log(req.body.action.data.listBefore);
+  console.log(req.body.action.data.listAfter);
   // if(z.action.display.translationKey === 'action_move_card_from_list_to_list'){
   //   const text = z.action.memberCreator.fullName+' has moved the card';
   //   const result = await write()
