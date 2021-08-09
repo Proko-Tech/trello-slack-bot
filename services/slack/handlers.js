f8a3b6dbe83f6ba9e2ec73ed1aaa0c1b6514cba3
Thomas Li <thomasligreat11@gmail.com>
2021-08-08 18:09:45 -0700

    fix: channel id

--- services/slack/handlers.js
+++ services/slack/handlers.js
@@ -1,7 +1,7 @@
 const web = require('./config');
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = process.env.C025SF41C2J;
+const conversationId = process.env.CHANNEL_ID;
 
 async function write(text){
     const res = await web.chat.postMessage({ channel: conversationId, text });
