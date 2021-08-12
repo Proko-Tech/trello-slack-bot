ba9dad801829eebad4facc6209c0e4936fcd7c22
Thomas Li <thomasligreat11@gmail.com>
2021-08-11 17:29:51 -0700

    fix: fixed channel

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.head('/card', function(req, res, next){
 
 router.post('/card', async function(req, res, next){
   if(req.body.action.display.translationKey === 'action_move_card_from_list_to_list'){
-    const text = `<@${process.env.CHANNEL_ID}> `+req.body.action.memberCreator.fullName+' has moved the card '+req.body.action.data.card.name+' from '+req.body.action.data.listBefore.name +' to '+req.body.action.data.listAfter.name+'. Check it out at: https://trello.com/c/'+req.body.action.data.card.shortLink;
+    const text = `<@channel> `+req.body.action.memberCreator.fullName+' has moved the card '+req.body.action.data.card.name+' from '+req.body.action.data.listBefore.name +' to '+req.body.action.data.listAfter.name+'. Check it out at: https://trello.com/c/'+req.body.action.data.card.shortLink;
     const result = await write(text);
   }
   res.status(200).json({status: 'success'});
